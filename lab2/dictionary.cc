cdeabb659408e162b17caeda2662d54519ae4f8b
Oskar Karlsson <oskar000202@gmail.com>
2023-11-14 21:36:05 +0100

    A2 done

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -10,10 +10,21 @@ using std::string;
 using std::vector;
 
 Dictionary::Dictionary() {
+	string tmp;
+	std::ifstream in("./words.txt");
+	
+	if(!in.is_open()){
+		throw std::runtime_error("Unable to open file ./words.txt");
+	}
+
+	while(getline(in,tmp)){
+		words.insert(tmp.substr(0, tmp.find_first_of(" ")));
+	}
+	in.close();
 }
 
 bool Dictionary::contains(const string& word) const {
-	return true;
+	return words.find(word) != words.end();
 }
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
