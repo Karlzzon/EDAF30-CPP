9d842af549943ac96078258a752913a5dad1bd23
Oskar Karlsson <oskar000202@gmail.com>
2023-11-14 20:20:11 +0100

    began lab2,A1 done

---
+++ lab2/word_processor.cc
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ifstream input("/usr/share/dict/words");
+	ofstream output;
+	output.open("words.txt");
+
+	vector<string> trigrams;
+	
+	string word;
+	while(getline(input,word))
+	{	
+		if(word.find('\'') == string::npos){
+			transform(word.begin(), word.end(), word.begin(), ::tolower);
+			output<<word + " ";
+			trigrams.clear();
+
+			for(string::size_type i=0; i+2<word.size();++i){
+				trigrams.push_back(word.substr(i,3));
+			}			
+			auto x = trigrams.size();
+			output << x << " ";
+			sort(trigrams.begin(),trigrams.end());
+
+			for ( auto x : trigrams){
+				output << x <<" ";	
+			}
+			output<< endl;
+		}	
+	}
+	
+	output.close();
+
+}
---
+++ lab2/word_processor.d
@@ -0,0 +1,1 @@
+word_processor.o: word_processor.cc
